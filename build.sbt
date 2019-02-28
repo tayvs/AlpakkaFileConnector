3ce8e500cec39fb1b83e9b12159e65dbd0a02560
Tayvs <xakDeltaForseXYZ>
2019-03-01 00:04:07 +0200

    Make parser persistence

--- build.sbt
+++ build.sbt
@@ -4,7 +4,12 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
+resolvers += "dnvriend" at "http://dl.bintray.com/dnvriend/maven"
+
 libraryDependencies += "com.typesafe.akka" %% "akka-persistence" % "2.5.21"
 libraryDependencies += "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8"
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.21"
 libraryDependencies += "com.lightbend.akka" %% "akka-stream-alpakka-file" % "1.0-M2"
+
+// akka 2.5.x
+libraryDependencies += "com.github.dnvriend" %% "akka-persistence-inmemory" % "2.5.15.1" % Test
--- src/main/scala/test/alpakka/file/connectors/Parser.scala
+++ src/main/scala/test/alpakka/file/connectors/Parser.scala
@@ -5,63 +5,71 @@ import java.nio.file.FileSystems
 
 import akka.NotUsed
 import akka.actor.Actor
+import akka.pattern._
 import akka.persistence.{PersistentActor, RecoveryCompleted}
 import akka.stream.ActorMaterializer
 import akka.stream.alpakka.file.scaladsl.FileTailSource
-import akka.stream.scaladsl.{FileIO, Framing}
-import akka.util.ByteString
+import akka.stream.scaladsl.{Framing, Keep, Sink}
+import akka.util.{ByteString, Timeout}
+import test.alpakka.file.connectors.Parser.{IncOffset, Ok, PersistedEvents}
 
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
-class Parser extends Actor {
-
+class Parser extends PersistentActor {
   import context.dispatcher
+  override def persistenceId: String = "parser"
 
   implicit val mat: ActorMaterializer = ActorMaterializer()
+  implicit val timeout: Timeout = Timeout(2 seconds)
   val fileName = "resources/file"
-
-  //  override def persistenceId: String = "parser"
-  //
-  //  override def receiveRecover: Receive = {
-  //    case RecoveryCompleted =>
-  //      val fs = FileSystems.getDefault
-  //      FileTailSource(
-  //        path = fs.getPath(fileName),
-  //        maxChunkSize = 8192,
-  //        startingPosition = 0,
-  //        pollingInterval = 250.millis
-  //      )
-  //        .via(Framing.delimiter(ByteString("\n"), Int.MaxValue))
-  //        .map(_.utf8String)
-  //        .runForeach(self ! _)
-  //        .foreach(_ => println("stream ended"))
-  //  }
-  //
-  //  override def receiveCommand: Receive = {
-  //    case str: String => println(str)
-  //  }
-
   var offset = 0l
 
-  val fs = FileSystems.getDefault
-  FileTailSource(
-    path = fs.getPath(fileName),
-    maxChunkSize = 0,
-    startingPosition = offset,
-    pollingInterval = 250.millis
-  )
-    .via(Framing.delimiter(ByteString("\n"), 100, true))
-    .map(_.utf8String)
-    .runForeach(self ! _)
-    .andThen {
-      case Success(value) => println(s"stream ended. $value")
-      case Failure(ex)    => ex.printStackTrace()
-    }
-
-  override def receive: Receive = {
-    case str: String =>
-      offset += str.length + 1
-      println(s"str $str offset $offset")
+  def updateState(ev: PersistedEvents): Unit = ev match {
+    case snapshot: Parser.Offset      => offset = snapshot.offset
+    case incOffset: Parser.IncOffset  => offset += incOffset.offsetDelta
   }
+
+  override def receiveRecover: Receive = {
+    case RecoveryCompleted =>
+      val fs = FileSystems.getDefault
+      FileTailSource(
+        path = fs.getPath(fileName),
+        maxChunkSize = 8192,
+        startingPosition = 0,
+        pollingInterval = 250.millis
+      )
+        .via(Framing.delimiter(ByteString("\n"), Int.MaxValue))
+        .map(_.utf8String)
+        .mapAsync(1) { line => (self ? line).mapTo[Ok.type] }
+        .toMat(Sink.ignore)(Keep.right)
+        .run()
+        .andThen {
+          case Success(value) => println(s"stream ended. $value")
+          case Failure(ex) => ex.printStackTrace()
+        }
+    case ev: PersistedEvents => updateState(ev)
+  }
+
+  override def receiveCommand: Receive = {
+    case str: String =>
+      persist(IncOffset(str.length + 1)) { ev =>
+        updateState(ev)
+        sender ! Ok
+        println(s"str $str offset $offset")
+      }
+  }
+}
+
+object Parser {
+
+  case object Ok
+
+  sealed trait PersistedEvents
+  sealed trait Snapshot extends PersistedEvents
+  sealed trait Events extends PersistedEvents
+
+  case class IncOffset(offsetDelta: Int) extends Events
+  case class Offset(offset: Long) extends Snapshot
+
 }
